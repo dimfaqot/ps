53461d09960cab8db557f790eeba13801e5d1cb4
dimfaqot <dimyfaqot@gmail.com>
2024-07-29 22:07:12 +0700

    tambah laporan di home

--- app/Views/billiard.php
+++ app/Views/billiard.php
@@ -14,7 +14,7 @@
                 <div class="col-md-6">
                     <h6 class="judul"><?= $m['meja']; ?></h6>
                     <?php for ($i = 1; $i < 25; $i++) : ?>
-                        <?php if ($i % 6 == 0) : ?>
+                        <?php if ($i == 1) : ?>
                             <div class="div_card mb-2">
                                 <table class="table table-sm table-bordered">
                                     <thead>
@@ -25,6 +25,35 @@
                                         </tr>
                                     </thead>
                                     <tbody>
+                                        <?php for ($y = 1; $y < ($i + 5); $y++) : ?>
+                                            <?php $jam = (strlen($y) <= 1 ? '0' . $y : $y); ?>
+                                            <tr>
+                                                <th style="text-align: center;" scope="row"><?= $jam . ".00"; ?></th>
+                                                <?php foreach ($data as $d) : ?>
+                                                    <?php if ($d['hari'] == hari(date('l'))['indo'] && $y == $d['jam'] && $d['meja'] == $m['meja']) : ?>
+                                                        <td><?= $d['pemesan']; ?></td>
+                                                        <td style="text-align: center;"><input data-biaya="<?= get_harga_billiard(); ?>" data-id="<?= $d['id']; ?>" class="form-check-input biaya" type="checkbox" value="" <?= $d['pemesan'] == '' || billiard_paid($d['id']) ? 'disabled' : ''; ?>> <?= rupiah(get_harga_billiard()); ?></td>
+                                                    <?php endif; ?>
+                                                <?php endforeach; ?>
+                                            </tr>
+                                        <?php endfor; ?>
+                                    </tbody>
+                                </table>
+                            </div>
+                        <?php endif; ?>
+                        <?php if ($i % 6 == 0) : ?>
+
+                            <div class="div_card mb-2">
+                                <table class="table table-sm table-bordered">
+                                    <thead>
+                                        <tr>
+                                            <th style="text-align: center;" scope="col">Jam</th>
+                                            <th style="text-align: center;" scope="col">Pemesan</th>
+                                            <th style="text-align: center;" scope="col">Biaya</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+
                                         <?php for ($x = $i; $x < ($i + 6); $x++) : ?>
                                             <?php if ($x < 25) : ?>
                                                 <?php $jam = (strlen($x) <= 1 ? '0' . $x : $x); ?>
