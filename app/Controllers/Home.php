1c686f52a1f2f6b12193f3a2753592f0360ecf1a
dimfaqot <dimyfaqot@gmail.com>
2024-08-03 15:04:45 +0700

    fix home

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -24,8 +24,13 @@ class Home extends BaseController
         $q = $db->get()->getResultArray();
 
         $data_tahun = [];
+        $data_tahun_p = [];
 
-        // mencari tahun
+        $tbl_pengeluaran = ($tabel == 'rental' ? 'inventaris' : 'pengeluaran_' . $tabel);
+        $dbp = db($tbl_pengeluaran);
+        $p = $dbp->get()->getResultArray();
+
+        // mencari tahun pemasukan
         foreach ($q as $i) {
             if ($tahun == 'All') {
                 $data_tahun[] = $i;
@@ -35,6 +40,19 @@ class Home extends BaseController
                 }
             }
         }
+        // mencari tahun pengeluaran
+        foreach ($p as $i) {
+            if ($tahun == 'All') {
+                $data_tahun_p[] = $i;
+            } else {
+                if ($tahun == date('Y', $i['tgl'])) {
+                    $data_tahun_p[] = $i;
+                }
+            }
+        }
+
+
+
 
         // mencari bulan
 
@@ -56,6 +74,21 @@ class Home extends BaseController
             $res[] = ['bulan' => $b['satuan'], 'data' => $temp, 'total' => $total];
         }
 
-        sukses_js('Connection success.', $res);
+        $res_p = [];
+
+        foreach (bulan() as $b) {
+            $temp = [];
+            $total = 0;
+            foreach ($data_tahun_p as $i) {
+                $i['tanggal'] = date('d/m/Y', $i['tgl']);
+                if ($b['angka'] == date('m', $i['tgl'])) {
+                    $temp[] = $i;
+                    $total +=  $i['harga'];
+                }
+            }
+            $res_p[] = ['bulan' => $b['satuan'], 'data' => $temp, 'total' => $total];
+        }
+
+        sukses_js('Connection success.', $res, $res_p);
     }
 }
--- app/Views/home.php
+++ app/Views/home.php
@@ -5,11 +5,15 @@
 
 
 <div class="container">
+
     <div class="row g-3">
         <div class="col-md-6">
             <div class="div_card bg_primary border border-primary text-white" style="border-radius:5px;">
                 <div class="d-flex justify-content-between">
-                    <h6><i class="fa-brands fa-playstation"></i> PENDAPATAN PS</h6>
+                    <h6>
+                        <i class="fa-brands fa-playstation mb-1"></i> PENDAPATAN PS
+                        <div style="font-weight: normal;font-size:x-small" class="total_rental"></div>
+                    </h6>
                     <h6 class="d-flex gap-1">
                         <select class="form-select get_pendapatan" data-tabel="rental">
                             <?php foreach (get_tahuns('rental') as $i) : ?>
@@ -28,7 +32,10 @@
         <div class="col-md-6">
             <div class="div_card bg_success border border-success text-white" style="border-radius:5px;">
                 <div class="d-flex justify-content-between">
-                    <h6><i class="fa-solid fa-bowling-ball"></i> PENDAPATAN BILLIARD</h6>
+                    <h6>
+                        <i class="fa-solid fa-bowling-ball mb-1"></i> PENDAPATAN BILLIARD
+                        <div style="font-weight: normal;font-size:x-small" class="total_billiard"></div>
+                    </h6>
                     <h6 class="d-flex gap-1">
                         <select class="form-select get_pendapatan" data-tabel="billiard">
                             <?php foreach (get_tahuns('billiard') as $i) : ?>
@@ -45,9 +52,12 @@
 
         </div>
         <div class="col-md-6">
-            <div class="div_card bg_success border border-success text-white" style="border-radius:5px;">
+            <div class="div_card bg_purple border border-success text-white" style="border-radius:5px;">
                 <div class="d-flex justify-content-between">
-                    <h6><i class="fa-solid fa-bowling-ball"></i> PENDAPATAN KANTIN</h6>
+                    <h6>
+                        <i class="fa-solid fa-shop mb-1"></i> PENDAPATAN KANTIN
+                        <div style="font-weight: normal;font-size:x-small" class="total_kantin"></div>
+                    </h6>
                     <h6 class="d-flex gap-1">
                         <select class="form-select get_pendapatan" data-tabel="kantin">
                             <?php foreach (get_tahuns('kantin') as $i) : ?>
@@ -88,6 +98,91 @@
 
     })
 
+    const content_table = (order, tabel, data, index) => {
+        let html = '';
+
+        html += '<table class="table table-striped table-bordered table-sm">';
+        html += '<thead>';
+        html += '<tr>';
+        html += '<th style="text-align: center;" scope="row">#</th>';
+        html += '<th style="text-align: center;" scope="row">Tgl</th>';
+        if (tabel == 'kantin') {
+            html += '<th style="text-align: center;" scope="row">Qty</th>';
+            html += '<th style="text-align: center;" scope="row">Harga' + (order == 'pemasukan' ? ' Satuan' : '') + '</th>';
+        } else {
+            if (order == 'pemasukan') {
+                html += '<th style="text-align: center;" scope="row">Meja</th>';
+                html += '<th style="text-align: center;" scope="row">Durasi</th>';
+
+            } else {
+                html += '<th style="text-align: center;" scope="row">Qty</th>';
+                html += '<th style="text-align: center;" scope="row">Harga</th>';
+
+            }
+
+        }
+        if (order == 'pemasukan') {
+            html += '<th style="text-align: center;" scope="row">Diskon</th>';
+            html += '<th style="text-align: center;" scope="row">' + (tabel == 'kantin' ? 'Harga' : 'Biaya') + '</th>';
+
+        } else {
+            html += '<th style="text-align: center;" scope="row">Pj</th>';
+        }
+        html += '</tr>';
+        html += '</thead>';
+        html += '<tbody>';
+        let total_m = 0;
+        data.forEach((val, idx) => {
+            if (val.bulan == index) {
+                total_m = val.total;
+                val.data.forEach((e, i) => {
+                    html += '<tr>';
+                    html += '<td>' + (i + 1) + '</td>';
+                    html += '<td style="text-align:center">' + e.tanggal + '</td>';
+                    if (tabel == 'kantin') {
+                        html += '<td style="text-align:center">' + e.qty + '</td>';
+                        html += '<td style="text-align:right">' + angka((order == 'pemasukan' ? e.harga_satuan : e.harga)) + '</td>';
+                    } else {
+                        if (order == 'pemasukan') {
+                            html += '<td>' + e.meja + '</td>';
+                            html += '<td>' + e.durasi + '</td>';
+
+                        } else {
+                            html += '<td style="text-align:center">' + e.qty + '</td>';
+                            html += '<td style="text-align:right">' + angka(e.harga) + '</td>';
+
+                        }
+
+                    }
+                    if (order == 'pemasukan') {
+
+                        html += '<td style="text-align:right">' + angka(e.diskon) + '</td>';
+                        html += '<td style="text-align:right">' + angka(e.biaya) + '</td>';
+                    } else {
+                        html += '<td>' + (tabel == 'rental' && order == 'pemasukan' ? e.petugas : (tabel == 'rental' && order == 'pengeluaran' ? e.pembeli : e.pj)) + '</td>';
+
+                    }
+                    html += '</tr>';
+                })
+            }
+        })
+
+        html += '<tr>';
+        html += '<th style="text-align:right" colspan="' + (order == 'pemasukan' ? 5 : 4) + '">TOTAL</th>';
+        html += '<th style="text-align:right">' + angka(total_m) + '</th>';
+        html += '</td>';
+
+        html += '</tbody>';
+        html += '</table>';
+
+        let res = {
+            total_m,
+            html
+        }
+        return res;
+
+    }
+
     const chart_html = (tabel, tahun) => {
 
         const bulans = <?= json_encode(bulan()); ?>;
@@ -101,6 +196,20 @@
             tahun
         }).then(res => {
             if (res.status == '200') {
+                // total pemasukan
+                let total_m = 0;
+                res.data.forEach((val, idx) => {
+                    total_m += val.total;
+                })
+
+                // total pengeluaran
+                let total_p = 0;
+                res.data2.forEach(e => {
+                    total_p += e.total;
+                })
+                $('.total_' + tabel).text(angka(total_m) + ' - ' + angka(total_p) + ' = ' + ((total_m - total_p) < 0 ? '-' : '') + angka((total_m - total_p).toString()));
+
+
                 valueX = [];
 
                 res.data.forEach(e => {
@@ -127,59 +236,57 @@
                             display: false
                         },
                         onClick: (e, values) => {
-                            let html = '';
-                            html += '<table class="table table-striped table-bordered table-sm">';
-                            html += '<thead>';
-                            html += '<tr>';
-                            html += '<th style="text-align: center;" scope="row">#</th>';
-                            html += '<th style="text-align: center;" scope="row">Tgl</th>';
-                            if (tabel == 'kantin') {
-                                html += '<th style="text-align: center;" scope="row">Harga Satuan</th>';
-                                html += '<th style="text-align: center;" scope="row">Qty</th>';
-                            } else {
-                                html += '<th style="text-align: center;" scope="row">Meja</th>';
-                                html += '<th style="text-align: center;" scope="row">Durasi</th>';
+                            let index = values[0]['_index'] + 1;
+                            let body_table = content_table('pemasukan', tabel, res.data, index);
 
-                            }
-                            html += '<th style="text-align: center;" scope="row">Diskon</th>';
-                            html += '<th style="text-align: center;" scope="row">' + (tabel == 'kantin' ? 'Harga' : 'Biaya') + '</th>';
-                            html += '</tr>';
-                            html += '</thead>';
-                            html += '<tbody>';
-                            let total = 0;
-                            res.data.forEach((val, idx) => {
-                                if (val.bulan == (values[0]._index + 1)) {
-                                    total = val.total;
-                                    val.data.forEach((e, i) => {
-                                        html += '<tr>';
-                                        html += '<td>' + (i + 1) + '</td>';
-                                        html += '<td style="text-align:center">' + e.tanggal + '</td>';
-                                        if (tabel == 'kantin') {
-                                            html += '<td style="text-align:right">' + angka(e.harga_satuan) + '</td>';
-                                            html += '<td style="text-align:center">' + e.qty + '</td>';
-                                        } else {
-                                            html += '<td>' + e.meja + '</td>';
-                                            html += '<td>' + e.durasi + '</td>';
-
-                                        }
-                                        html += '<td style="text-align:right">' + angka(e.diskon) + '</td>';
-                                        html += '<td style="text-align:right">' + angka(e.biaya) + '</td>';
-                                        html += '</tr>';
-                                    })
-                                }
-                            })
-
-                            html += '<tr>';
-                            html += '<th style="text-align:right" colspan="5">TOTAL</th>';
-                            html += '<th style="text-align:right">' + angka(total) + '</th>';
-                            html += '</td>';
-
-                            html += '</tbody>';
-                            html += '</table>';
+                            let html = '<div class="d-flex justify-content-between judul">';
+                            html += '<div>';
+                            html += 'DETAIL ' + tabel.toUpperCase();
+                            html += '</div>';
+                            html += '<div>';
+                            html += '<a type="button" href="" class="text_danger" data-bs-dismiss="modal"><i class="fa-solid fa-circle-xmark"></i></a>';
+                            html += '</div>';
+                            html += '</div>';
+                            html += '<div style="border-radius:5px;" class="judul_modal judul_' + tabel + '"></div>';
+                            html += '<ul class="nav nav-tabs">';
+                            html += '<li class="nav-item">';
+                            html += '<a class="nav-link active detail_data" data-order="pemasukan" aria-current="page" href="#">Pemasukan</a>';
+                            html += '</li>';
+                            html += '<li class="nav-item">';
+                            html += '<a class="nav-link detail_data" data-order="pengeluaran" aria-current="page" href="#">Pengeluaran</a>';
+                            html += '</li>';
+                            html += '</ul>';
+                            html += '<div class="content_table">';
+                            html += body_table.html;
+                            html += '</div>';
                             $('.body_detail_pendapatan').html(html);
                             let myModal = document.getElementById('detail_pendapatan');
                             let modal = bootstrap.Modal.getOrCreateInstance(myModal)
                             modal.show();
+
+                            let total_p = 0;
+                            res.data2.forEach((val, idx) => {
+                                if (val.bulan == index) {
+                                    total_p = val.total;
+                                }
+                            })
+
+                            $('.judul_' + tabel).text(angka(body_table.total_m) + ' - ' + angka(total_p) + ' = ' + ((body_table.total_m - total_p) < 0 ? '-' : '') + angka(body_table.total_m - total_p));
+
+                            $(document).on('click', '.detail_data', function(e) {
+                                e.preventDefault();
+                                let order = $(this).data('order');
+                                let content = content_table(order, tabel, (order == 'pemasukan' ? res.data : res.data2), index);
+
+                                let elem = document.querySelectorAll('.detail_data');
+
+                                elem.forEach(e => {
+                                    e.classList.remove('active');
+                                })
+                                $(this).addClass('active');
+                                $('.content_table').html(content.html);
+                            })
+
                             // let datasetIndex = activeEls[0].datasetIndex;
                             // let dataIndex = activeEls[0].index;
                             // let datasetLabel = e.chart.data.datasets[datasetIndex].label;
