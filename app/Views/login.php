6adeb1615ab182e6d45bbd6446f80461e755294b
dimfaqot <dimyfaqot@gmail.com>
2024-11-17 11:22:04 +0700

    update map login page

--- app/Views/login.php
+++ app/Views/login.php
@@ -21,57 +21,66 @@
 
 
     const successCb = (position) => {
+
+
         let maphtml = '';
         let latitude = position.coords.latitude;
         let longitude = position.coords.longitude;
 
-        maphtml += '<p>Latitude: ' + latitude + ' Longitude: ' + longitude + '</p>';
-        maphtml += '<iframe width="100%" height="300" src="https://maps.google.com/maps?q=' + latitude + ',' + longitude + '&amp;z=15&amp;output=embed"></iframe>';
+        console.log(latitude + ' > ' + lat_bawah + ' = ' + (latitude > lat_bawah));
+        console.log(latitude + ' < ' + lat_atas + ' = ' + (latitude > lat_atas));
+        console.log(longitude + ' > ' + long_atas + ' = ' + (longitude > long_atas));
+        console.log(longitude + ' < ' + long_bawah + ' = ' + (longitude > long_bawah));
+        if (latitude > lat_bawah && latitude < lat_atas && longitude > long_atas && longitude < lat_bawah) {
 
-        $('.map_lokasi_saya').html(maphtml);
+            maphtml += '<p>Latitude: ' + latitude + ' Longitude: ' + longitude + '</p>';
+            maphtml += '<iframe width="100%" height="300" src="https://maps.google.com/maps?q=' + latitude + ',' + longitude + '&amp;z=15&amp;output=embed"></iframe>';
+            $('.map_lokasi_saya').html(maphtml);
 
-        if (latitude > lat_atas && latitude < lat_bawah) {
-            $('.body_login').text("(lat) LOKASIMU DI LUAR AREA!.");
-            return false;
+            let html = '';
+            html += '<div class="d-flex gap-2">';
+            html += '<div style="padding:40px;" class="flex-fill">';
+            html += '<form action="<?= base_url('auth'); ?>" method="post">';
+            html += '<input type="hidden" class="ip" name="ip">';
+            html += '<input type="hidden" class="latitude" name="latitude" value="' + latitude + '">';
+            html += '<input type="hidden" class="longitude" name="longitude" value="' + longitude + '">';
+            html += '<div class="input-group input-group-sm mb-2">';
+            html += '<span class="input-group-text"><i style="color:#0e8aed;" class="fa-solid fa-user-large"></i></span>';
+            html += '<input type="text" class="form-control" name="username" placeholder="Username">';
+            html += '</div>';
+            html += '<div class="input-group input-group-sm mb-2">';
+            html += '<span class="input-group-text"><i style="color:#0e8aed;" class="fa-solid fa-lock"></i></span>';
+            html += '<input type="password" class="form-control" name="password" placeholder="Password">';
+            html += '</div>';
+            html += '<div class="d-grid">';
+            html += '<button type="submit" class="btn_info">LOGIN</button>';
+            html += '</div>';
+            html += '</form>';
+
+            html += '</div>';
+
+
+            html += '<div class="flex-fill d-md-block d-none px-4 py-3" style="background: rgb(124,197,255);background: linear-gradient(90deg, rgba(124,197,255,1) 31%, rgba(220,239,255,1) 100%);padding:6px;border-radius:0px 10px 10px 0px">';
+            html += '<h5 style="text-align: center;background-color:#fff;border-radius:20px;padding:10px"><i style="color: #61b4ff;" class="fa-solid fa-gamepad"></i> <span style="color: #054d8e;">PS MANIA</span></h5>';
+            html += '<p style="text-align: center;font-size:small">"If you do what you love, it is the best way to relax." </p>';
+            html += '<hr>';
+            html += '<h1 style="text-align: center;"><i class="fa-brands fa-playstation"></i></h1>';
+            html += '</div>';
+            html += '</div>';
+            $('.body_login').html(html);
+        } else {
+
+            $('.body_login').html('<div class="bg_danger rounded m-2"><div class="p-2 fw-bold text-light"><i class="fa-solid fa-triangle-exclamation"></i> LOKASIMU DI LUAR AREA!.</div></div>');
+
         }
-
-        if (longitude > long_atas && longitude < lat_bawah) {
-            $('.body_login').text("(long) LOKASIMU DI LUAR AREA!.");
-            return false;
-        }
-
-        let html = '';
-        html += '<div class="d-flex gap-2">';
-        html += '<div style="padding:40px;" class="flex-fill">';
-        html += '<form action="<?= base_url('auth'); ?>" method="post">';
-        html += '<input type="hidden" class="ip" name="ip">';
-        html += '<input type="hidden" class="latitude" name="latitude" value="' + latitude + '">';
-        html += '<input type="hidden" class="longitude" name="longitude" value="' + longitude + '">';
-        html += '<div class="input-group input-group-sm mb-2">';
-        html += '<span class="input-group-text"><i style="color:#0e8aed;" class="fa-solid fa-user-large"></i></span>';
-        html += '<input type="text" class="form-control" name="username" placeholder="Username">';
-        html += '</div>';
-        html += '<div class="input-group input-group-sm mb-2">';
-        html += '<span class="input-group-text"><i style="color:#0e8aed;" class="fa-solid fa-lock"></i></span>';
-        html += '<input type="password" class="form-control" name="password" placeholder="Password">';
-        html += '</div>';
-        html += '<div class="d-grid">';
-        html += '<button type="submit" class="btn_info">LOGIN</button>';
-        html += '</div>';
-        html += '</form>';
-
-        html += '</div>';
-
-
-        html += '<div class="flex-fill d-md-block d-none px-4 py-3" style="background: rgb(124,197,255);background: linear-gradient(90deg, rgba(124,197,255,1) 31%, rgba(220,239,255,1) 100%);padding:6px;border-radius:0px 10px 10px 0px">';
-        html += '<h5 style="text-align: center;background-color:#fff;border-radius:20px;padding:10px"><i style="color: #61b4ff;" class="fa-solid fa-gamepad"></i> <span style="color: #054d8e;">PS MANIA</span></h5>';
-        html += '<p style="text-align: center;font-size:small">"If you do what you love, it is the best way to relax." </p>';
-        html += '<hr>';
-        html += '<h1 style="text-align: center;"><i class="fa-brands fa-playstation"></i></h1>';
-        html += '</div>';
-        html += '</div>';
-        $('.body_login').html(html);
         $('.body_login').show();
+
+        // if (longitude > long_atas && longitude < lat_bawah) {
+        //     $('.body_login').text("(long) LOKASIMU DI LUAR AREA!.");
+        //     return false;
+        // }
+
+
     }
 
     const errorCb = (error) => {
