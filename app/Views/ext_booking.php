7babfb6a84c46d8c61569e2eba69b62b77c4b581
dimfaqot <dimyfaqot@gmail.com>
2025-01-05 00:26:29 +0700

    fitur panel

--- app/Views/ext_booking.php
+++ app/Views/ext_booking.php
@@ -221,7 +221,7 @@ $billiard = $db->orderBy('meja', 'ASC')->get()->getResultArray();
             }
             html_ps += '<div class="rounded-circle embos2 text-center fw-bold btn_meja_' + e.meja + ' btn_meja ' + (e.is_active == 1 ? 'active' : 'default') + '" data-meja="' + e.meja + '" data-menu="Ps" data-is_active="' + e.is_active + '" style="cursor:pointer;padding:13px 5px 5px 6px;font-size:35px;width: 85px;height:85px;color:#7c6f3e;border:1px solid #fce882">';
             html_ps += '<div class="text-center" style="font-size:9px;margin-bottom:-2px">MEJA</div>' + e.meja;
-            html_ps += '<div class="text-center div_durasi_' + e.meja + '" style="font-size:9px;margin-top:-5px"></div>';
+            html_ps += '<div class="text-center div_durasi_' + e.meja + '" style="font-size:9px;margin-top:-5px">Available</div>';
             html_ps += '</div>';
             if (i % 4 == 3) {
                 html_ps += '</div>';
@@ -233,7 +233,7 @@ $billiard = $db->orderBy('meja', 'ASC')->get()->getResultArray();
             }
             html_billiard += '<div class="rounded-circle embos2 text-center fw-bold btn_meja_' + e.meja + ' btn_meja ' + (e.is_active == 1 ? 'active' : 'default') + '" data-meja="' + e.meja + '" data-menu="Billiard" data-is_active="' + e.is_active + '" style="cursor:pointer;padding:13px 5px 5px 6px;font-size:35px;width: 85px;height:85px;color:#7c6f3e;border:1px solid #fce882">';
             html_billiard += '<div class="text-center" style="font-size:9px;margin-bottom:-2px">MEJA</div>' + e.meja;
-            html_billiard += '<div class="text-center div_durasi_' + e.meja + '" style="font-size:9px;margin-top:-5px"></div>';
+            html_billiard += '<div class="text-center div_durasi_' + e.meja + '" style="font-size:9px;margin-top:-5px">Available</div>';
             html_billiard += '</div>';
             if (i % 4 == 3) {
                 html_billiard += '</div>';
