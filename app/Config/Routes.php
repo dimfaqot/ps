b12097d0252b70dc490e025860ab324f501ce870
dimfaqot <dimyfaqot@gmail.com>
2025-01-16 23:13:44 +0700

    ble

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -48,6 +48,9 @@ $routes->post('/finger/Absen', 'Finger::absen');
 $routes->post('/finger/add_message', 'Finger::add_message');
 $routes->post('/finger/Add', 'Finger::add');
 
+// uuid ble
+$routes->post('/ble/settings', 'Ble::settings');
+$routes->post('/ble/perangkat', 'Ble::perangkat');
 
 // bot
 $routes->get('/api/wabot', 'Api::wabot');
---
+++ app/Controllers/Ble.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Controllers;
+
+class Ble extends BaseController
+{
+    public function settings()
+    {
+        $jwt = $this->request->getVar('jwt');
+        $decode = decode_jwt_finger($jwt);
+        $nama_server = $decode['uid'];
+
+        $db = db('ble');
+
+        $q = $db->where('nama_server', $nama_server)->get()->getRowArray();
+
+        if (!$q) {
+            gagal_js("Nama server tidak ditemukan!.");
+        }
+
+        sukses_js("sukses", $q['uuid'], $q['karakteristik']);
+    }
+    public function perangkat()
+    {
+        $jwt = $this->request->getVar('jwt');
+        $decode = decode_jwt_finger($jwt);
+        $nama_server = $decode['uid'];
+
+        $db = db('perangkat');
+        $qp = $db->where('grup', $nama_server)->orderBy('no_urut', 'ASC')->get()->getResultArray();
+        if (!$qp) {
+            gagal_js("Nama server tidak ditemukan!.");
+        }
+        $data = [];
+        foreach ($qp as $i) {
+            $data[] = $i['status'] . $i['no_urut'];
+        }
+
+        if ($nama_server == "Billiard") {
+            $db = db('billiard_2');
+            $qb = $db->orderBy('meja', 'ASC')->get()->getResultArray();
+            if (!$qb) {
+                gagal_js("Nama server tidak ditemukan!.");
+            }
+
+            foreach ($qb as $i) {
+                $meja = ($i['meja'] == 1 ? 10 : ($i['meja'] == 2 ? 11 : $i['meja']));
+                $data[] = $i['is_active'] . $meja;
+            }
+
+
+            sukses_js("sukses", $data, count($qp), count($qb));
+        }
+    }
+}
