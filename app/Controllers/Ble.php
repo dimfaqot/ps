cacdefabf7df6435c4fa0d7574f8884708fba18e
dimfaqot <dimyfaqot@gmail.com>
2025-01-20 12:48:43 +0700

    wifi

--- app/Controllers/Ble.php
+++ app/Controllers/Ble.php
@@ -29,7 +29,7 @@ class Ble extends BaseController
 
         $res = [];
         // foreach($)
-        sukses_js("ok", $status_sekarang);
+        // sukses_js("ok", $status_sekarang);
 
         $db = db('perangkat');
         $qp = $db->where('grup', ($nama_server == "Billiards" ? "Billiard" : $nama_server))->orderBy('no_urut', 'ASC')->get()->getResultArray();
