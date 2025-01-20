77aacfc587a4bd7c2abc9d8e48b493e76e4cdc8c
dimfaqot <dimyfaqot@gmail.com>
2025-01-20 17:56:53 +0700

    ble server

--- app/Views/ext_booking.php
+++ app/Views/ext_booking.php
@@ -1,5 +1,76 @@
 <?php
 
+$nama_server = "Billiards";
+$status_esp = "[21]";
+// $status_esp = "";
+
+$jml_perangkat = 0;
+$jml_meja = 0;
+
+$db = db('perangkat');
+$qp = $db->where('grup', ($nama_server == "Billiards" ? "Billiard" : $nama_server))->orderBy('no_urut', 'ASC')->get()->getResultArray();
+if (!$qp) {
+    gagal_js("Nama server tidak ditemukan!.");
+}
+
+$data = [];
+
+if ($nama_server == "Billiards") {
+    $dbj = db('jadwal_2');
+    if ($status_esp == "") {
+        foreach ($qp as $i) {
+            $hasil = $i['no_urut'] . $i['status'];
+            $data[] = (int)$hasil;
+        }
+        $jml_perangkat = count($qp);
+        $qb = $dbj->orderBy('meja', 'ASC')->get()->getResultArray();
+        if (!$qb) {
+            gagal_js("Nama server tidak ditemukan!.");
+        }
+
+        foreach ($qb as $i) {
+            $meja = ($i['meja'] == 1 ? 10 : ($i['meja'] == 2 ? 11 : $i['meja']));
+            $hasil = $meja . $i['is_active'];
+            $data[] =  (int)$hasil;
+        }
+
+        $jml_meja = count($qb);
+    } else {
+        $statusArr = stringArr_to_arr($status_esp);
+        $jml_mej = [];
+
+        $dbb = db('jadwal_2');
+        foreach ($statusArr as $i) {
+            $mej = ($i['perangkat'] == 10 ? 1 : ($i['perangkat'] == 11 ? 2 : $i['perangkat']));
+            $q = $dbb->where('meja', $mej)->get()->getRowArray();
+            if ($i['perangkat'] >= 10) {
+                if ($q && $mej == $q['meja'] && $q['is_active'] != $i['status']) {
+                    $meja = ($q['meja'] == 1 ? 10 : ($q['meja'] == 2 ? 11 : $q['meja']));
+                    $hasil = $meja . $q['is_active'];
+                    $jml_mej[] = $hasil;
+                    $data[] =  (int)$hasil;
+                }
+            }
+        }
+        $jml_meja = count($jml_mej);
+
+        $jml_per = [];
+        foreach ($statusArr as $i) {
+            $q = $db->where('grup', ($nama_server == "Billiards" ? "Billiard" : $nama_server))->where("no_urut", $i['perangkat'])->get()->getRowArray();
+
+            if ($q && $q['status'] != $i['status']) {
+                $hasil = $q['no_urut'] . $q['status'];
+                $jml_per[] = $hasil;
+                $data[] =  (int)$hasil;
+            }
+        }
+        $jml_perangkat = count($jml_per);
+    }
+    dd((count($data) == 0 || $data == null ? "" : $data));
+    sukses_js("sukses", (count($data) == 0 || $data == null ? "" : $data), $jml_perangkat, $jml_meja, $jml_perangkat + $jml_meja);
+}
+
+
 $db = db('unit');
 $q = $db->whereNotIn('status', ['Maintenance'])->orderBy('id', 'ASC')->get()->getResultArray();
 $dbr = db('rental');
